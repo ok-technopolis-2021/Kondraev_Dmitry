de7b1dc5a845360d7aa5dde15dc8e44b78cd3ae6
mariohuq <mariohuq@github.com>
2021-11-15 22:16:11 +0300

    finish implementation

--- src/app.js
+++ src/app.js
@@ -1,14 +1,5 @@
 import './common.blocks/theme-switcher/theme-switcher';
 
-const map = func => target => func(target);
-
-const define = (target, properties) => {
-    for (const [key, value] of Object.entries(properties)) {
-        target[key] = value;
-    }
-    return target;
-};
-
 class Skill {
     constructor(name = '', value = 0) {
         this.name = name;
@@ -16,8 +7,15 @@ class Skill {
     }
 
     render(target) {
+        const define = (target, properties) => {
+            for (const [key, value] of Object.entries(properties)) {
+                target[key] = value;
+            }
+            return target;
+        };
+
         target.className = 'skill';
-        target.style.setProperty('--value', this.valueFormat());
+        this.renderStyle(target);
         define(target.appendChild(document.createElement('span')), {
             className: 'skill__name'
         }).appendChild(document.createTextNode(this.name));
@@ -44,6 +42,14 @@ class Skill {
                 title: 'Remove skill'
             }
         );
+
+        target.querySelector('.remove-button').addEventListener('click', function (event) {
+            event.target.closest('.skill').remove();
+        });
+    }
+
+    renderStyle(target) {
+        target.style.setProperty('--skill-value', this.valueFormat());
     }
 
     valueFormat() {
@@ -51,17 +57,44 @@ class Skill {
     }
 }
 
+function addSkill(skill, skillList) {
+    skill.render(skillList.appendChild(document.createElement('div')));
+}
+
 window.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('.skill_form').addEventListener('submit', function (event) {
-        const [name, value] = [event.target.elements.skillName.value, event.target.elements.skillValue.value];
-        if (name === '') {
-            // incorrect name
-        }
-        (new Skill(name, value))
-            .render(document
-                .querySelector('.skills-list')
-                .appendChild(document.createElement('div'))
-            );
+    const skillList = document.querySelector('.skills-list');
+    if (!skillList) {
+        return;
+    }
+
+    // Здесь можно забрать скиллы с сервера
+    for (const skill of [
+        new Skill('JavaScript', 25),
+        new Skill('PHP', 85),
+        new Skill('HTML/CSS', 100),
+        new Skill('Cobol', -100)
+    ]) {
+        addSkill(skill, skillList);
+    }
+
+    const form = document.querySelector('.skill_form');
+    form.model = new Skill();
+    form.addEventListener('submit', function (event) {
+        addSkill(event.target.model, skillList);
+        form.reset();
+        form.elements[0].focus();
         event.preventDefault();
+    });
+    form.elements.skillValue.addEventListener('input', function (event) {
+        form.model.value = event.target.value;
+        form.model.renderStyle(form.querySelector('.skill'));
+    });
+    form.addEventListener('reset', function () {
+        form.model.value = 0;
+        form.model.name = '';
+        form.model.renderStyle(form.querySelector('.skill'));
+    });
+    form.elements.skillName.addEventListener('change', function (event) {
+        form.model.name = event.target.value;
     });
 });
