b7ccb98a82a86bfdb16e987e31ad7aa5b44c5e1a
mariohuq <mariohuq@github.com>
2021-12-01 18:23:15 +0300

    Refactor: make several components

---
+++ react-cv/src/components/MainInfo/MainInfo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export class MainInfo extends React.Component {
+    render() {
+        return <div className="main-info">
+            <h1 className="main-info__name">Kondraev Dmitry</h1>
+            <p className="main-info__position">Web Developer</p>
+        </div>;
+    }
+}
