ecd5019790c7ff2e96de0f51a3702315ecccb87e
mariohuq <mariohuq@github.com>
2021-12-23 23:26:48 +0300

    Remove test.

--- src/components/SkillsApp/SkillsApp.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import SkillsApp from './SkillsApp';
-
-test('renders learn react link', () => {
-  render(<SkillsApp />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
