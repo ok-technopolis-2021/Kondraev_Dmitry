fdb4315e8eeed46bbfe04e614466d18254fc006a
mariohuq <mariohuq@github.com>
2021-12-23 23:02:58 +0300

    Remove home link, add routing, add message style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from 'react-router-dom';
+import SkillsApp from "./components/SkillsApp/SkillsApp";
+import MainInfo from "./components/MainInfo/MainInfo";
 import ReactDOM from 'react-dom';
-import SkillsApp from './components/SkillsApp/SkillsApp';
 import reportWebVitals from './reportWebVitals';
 import registerSwitcher from "./common.blocks/theme-switcher/theme-switcher";
 
@@ -11,10 +17,15 @@ window.addEventListener('DOMContentLoaded', function () {
 });
 
 ReactDOM.render(
-  <React.StrictMode>
-    <SkillsApp/>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<MainInfo/>}/>
+                <Route path="skills" element={<SkillsApp/>}/>
+            </Routes>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
